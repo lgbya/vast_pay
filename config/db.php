2876c77c9c2295c513b381df5e32cbbb573119fa
lgbya <lgbya1105882406@aliyun.com>
2020-04-09 10:29:27 +0800

    test

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -20,7 +20,7 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout'],
+                'only' => ['logout' ],
                 'rules' => [
                     [
                         'actions' => ['logout'],
