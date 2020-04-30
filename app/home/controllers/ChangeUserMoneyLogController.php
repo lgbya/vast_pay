cd257f1b27316ff1f8f6f5e73c0a4374de3437e1
lgbya <lgbya1105882406@aliyun.com>
2020-04-30 15:51:53 +0800

    前台支付订单和资金日志

---
+++ app/home/controllers/ChangeUserMoneyLogController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace home\controllers;
+
+use Yii;
+use common\models\ChangeUserMoneyLog;
+use common\models\ChangeUserMoneyLogSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * ChangeUserMoneyLogController implements the CRUD actions for ChangeUserMoneyLog model.
+ */
+class ChangeUserMoneyLogController extends BaseController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all ChangeUserMoneyLog models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new ChangeUserMoneyLogSearch();
+        $lsQueryParam = Yii::$app->request->queryParams;
+        $lsQueryParam['ChangeUserMoneyLogSearch']['user_id'] = $this->user_id;
+        $dataProvider = $searchModel->search($lsQueryParam);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+
+    protected function findModel($id)
+    {
+        $model = ChangeUserMoneyLog::find()->where(['id'=>$id, 'user_id'=> $this->user_id])->one();
+        if ($model !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
+}
---
+++ app/home/controllers/PayOrderController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace home\controllers;
+
+use common\models\Product;
+use Yii;
+use common\models\PayOrder;
+use common\models\PayOrderSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * PayOrderController implements the CRUD actions for PayOrder model.
+ */
+class PayOrderController extends BaseController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all PayOrder models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $lsQueryParam = Yii::$app->request->queryParams;
+        $lsQueryParam['PayOrderSearch']['user_id'] = $this->user_id;
+
+        $searchModel = new PayOrderSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        $omProduct = new Product();
+        $lProductIdToName = $omProduct->getIdToNameList();
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'lProductIdToName' => $lProductIdToName,
+        ]);
+    }
+
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+
+    protected function findModel($id)
+    {
+        $model = PayOrder::find()->where(['id'=>$id, 'user_id'=> $this->user_id])->one();
+        if ($model !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
+}
