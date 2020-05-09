3d9145bf9cb8019e1b9cccc73b64e5456fd084a4
lgbya <lgbya1105882406@aliyun.com>
2020-05-09 18:50:16 +0800

    数据表修改

--- payment.sql
+++ payment.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50726
 File Encoding         : 65001
 
-Date: 2020-04-30 18:13:04
+Date: 2020-05-09 18:49:47
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,6 +25,8 @@ CREATE TABLE `p_admin` (
   `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pre_login_at` int(10) unsigned NOT NULL,
+  `pre_login_ip` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `status` smallint(6) NOT NULL DEFAULT '10',
   `created_at` int(11) NOT NULL,
@@ -35,7 +37,7 @@ CREATE TABLE `p_admin` (
 -- ----------------------------
 -- Records of p_admin
 -- ----------------------------
-INSERT INTO `p_admin` VALUES ('1', 'admin', 'eb2ceSlO_HduMj9dQWzk9rPJt2P0L-bJ', '$2y$13$X1i.6iOS61RMBKjAItz.8uAyVeaH7EiEJOqgs8ozvj7UM5rSkIe1W', '_N78d_egne3rxC-OPuCblPSXO8yZPJgK_1586831793', 'test@test.com', '10', '1586831793', '1586831793');
+INSERT INTO `p_admin` VALUES ('1', 'admin', 'eb2ceSlO_HduMj9dQWzk9rPJt2P0L-bJ', '$2y$13$X1i.6iOS61RMBKjAItz.8uAyVeaH7EiEJOqgs8ozvj7UM5rSkIe1W', '_N78d_egne3rxC-OPuCblPSXO8yZPJgK_1586831793', '1588751144', '127.0.0.1', 'test@test.com', '10', '1586831793', '1588751144');
 
 -- ----------------------------
 -- Table structure for `p_auth_assignment`
@@ -135,6 +137,8 @@ INSERT INTO `p_auth_item` VALUES ('/batch/cruds', '2', null, null, null, '148783
 INSERT INTO `p_auth_item` VALUES ('/batch/index', '2', null, null, null, '1487839853', '1487839853');
 INSERT INTO `p_auth_item` VALUES ('/batch/models', '2', null, null, null, '1487839853', '1487839853');
 INSERT INTO `p_auth_item` VALUES ('/change-user-money-log/index', '2', null, null, null, '1588151587', '1588151587');
+INSERT INTO `p_auth_item` VALUES ('/data-report/*', '2', null, null, null, '1588919018', '1588919018');
+INSERT INTO `p_auth_item` VALUES ('/data-report/product-analyze', '2', null, null, null, '1588919034', '1588919034');
 INSERT INTO `p_auth_item` VALUES ('/gii/*', '2', null, null, null, '1487816732', '1487816732');
 INSERT INTO `p_auth_item` VALUES ('/gii/default/*', '2', null, null, null, '1487816732', '1487816732');
 INSERT INTO `p_auth_item` VALUES ('/gii/default/action', '2', null, null, null, '1487816732', '1487816732');
@@ -275,26 +279,20 @@ DROP TABLE IF EXISTS `p_change_user_money_log`;
 CREATE TABLE `p_change_user_money_log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `change_money` int(11) NOT NULL DEFAULT '0',
-  `before_money` int(10) unsigned NOT NULL DEFAULT '0',
-  `after_money` int(10) unsigned NOT NULL DEFAULT '0',
+  `change_money` decimal(12,3) NOT NULL DEFAULT '0.000',
+  `before_money` decimal(12,3) unsigned NOT NULL DEFAULT '0.000',
+  `after_money` decimal(12,3) unsigned NOT NULL DEFAULT '0.000',
   `type` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `created_at` int(10) unsigned NOT NULL DEFAULT '0',
   `updated_at` int(10) unsigned NOT NULL DEFAULT '0',
   `extra` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
 -- Records of p_change_user_money_log
 -- ----------------------------
-INSERT INTO `p_change_user_money_log` VALUES ('17', '2', '11111', '0', '11111', '1', '1588147210', '1588147210', '12345678901234567890');
-INSERT INTO `p_change_user_money_log` VALUES ('18', '2', '-11111', '11111', '0', '2', '1588147519', '1588147519', '12345678901234567890');
-INSERT INTO `p_change_user_money_log` VALUES ('19', '2', '11111', '0', '11111', '1', '1588148573', '1588148573', '12345678901234567890');
-INSERT INTO `p_change_user_money_log` VALUES ('20', '2', '-11111', '11111', '0', '2', '1588148579', '1588148579', '12345678901234567890');
-INSERT INTO `p_change_user_money_log` VALUES ('21', '1', '11111', '0', '11111', '1', '1588148645', '1588148645', '12345678901234567890');
-INSERT INTO `p_change_user_money_log` VALUES ('22', '1', '-11111', '11111', '0', '2', '1588148706', '1588148706', '12345678901234567890');
-INSERT INTO `p_change_user_money_log` VALUES ('23', '1', '11111', '0', '11111', '1', '1588148711', '1588148711', '12345678901234567890');
+INSERT INTO `p_change_user_money_log` VALUES ('1', '10', '99.900', '0.000', '99.900', '3', '1589021126', '1589021126', '102020050918442689392');
 
 -- ----------------------------
 -- Table structure for `p_menu`
@@ -310,7 +308,7 @@ CREATE TABLE `p_menu` (
   PRIMARY KEY (`id`),
   KEY `parent` (`parent`),
   CONSTRAINT `p_menu_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `p_menu` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of p_menu
@@ -333,7 +331,10 @@ INSERT INTO `p_menu` VALUES ('15', '用户列表', '14', '/user/index', null, 0x
 INSERT INTO `p_menu` VALUES ('16', '订单管理', null, null, null, 0x627269656663617365);
 INSERT INTO `p_menu` VALUES ('17', '支付订单列表', '16', '/pay-order/index', null, 0x72656F7264657220);
 INSERT INTO `p_menu` VALUES ('18', '用户资金日志', '16', '/change-user-money-log/index', null, 0x726D62);
-INSERT INTO `p_menu` VALUES ('19', '数据分析', null, null, null, 0x6C696E652D6368617274);
+INSERT INTO `p_menu` VALUES ('19', '数据分析', null, null, '99', 0x6C696E652D6368617274);
+INSERT INTO `p_menu` VALUES ('20', '网站管理', null, null, null, 0x6465736B746F70);
+INSERT INTO `p_menu` VALUES ('21', '服务器信息', '20', '/site/index', null, 0x736572766572);
+INSERT INTO `p_menu` VALUES ('22', '产品分析', '19', '/data-report/product-analyze', '99', 0x617265612D6368617274);
 
 -- ----------------------------
 -- Table structure for `p_migration`
@@ -374,9 +375,9 @@ CREATE TABLE `p_pay_channel` (
 -- ----------------------------
 -- Records of p_pay_channel
 -- ----------------------------
-INSERT INTO `p_pay_channel` VALUES ('1', '3', 'a2222', '12321', '1', '1', '1', 'http://www.a.com', '1', '1587117867', '1588236028', '0');
+INSERT INTO `p_pay_channel` VALUES ('1', '3', 'a2222', 'Demo', '1', '1', '1', 'http://www.a.com', '1', '1587117867', '1588994375', '0');
 INSERT INTO `p_pay_channel` VALUES ('2', '5', 'd44', '12', '12321', '312', '31', 'http://www.a.com', '1', '1588039184', '1588236044', '1');
-INSERT INTO `p_pay_channel` VALUES ('3', '3', 'a222', '312333', '12321', '312', '31', 'http://www.a.com', '1', '1588039216', '1588235980', '0');
+INSERT INTO `p_pay_channel` VALUES ('3', '3', 'a222', 'Test', '12321', '312', '31', 'http://www.a.com', '1', '1588039216', '1588994320', '0');
 INSERT INTO `p_pay_channel` VALUES ('4', '3', 'a22', 'a2', '1', '1', '1', 'http://www.a.com', '1', '1588044444', '1588235959', '0');
 INSERT INTO `p_pay_channel` VALUES ('5', '3', 'a2', '3131', '31', '321', '131', 'http://www.a.com', '1', '1588044493', '1588235937', '0');
 INSERT INTO `p_pay_channel` VALUES ('6', '5', 'd4', '31', '31', '321', '31', 'http://www.a.com', '1', '1588044530', '1588236041', '1');
@@ -394,6 +395,7 @@ CREATE TABLE `p_pay_channel_account` (
   `md5_key` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `private_key` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `public_key` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `extra` varchar(255) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `weight` int(10) unsigned NOT NULL DEFAULT '0',
   `status` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `created_at` int(10) unsigned NOT NULL,
@@ -406,7 +408,7 @@ CREATE TABLE `p_pay_channel_account` (
 -- ----------------------------
 -- Records of p_pay_channel_account
 -- ----------------------------
-INSERT INTO `p_pay_channel_account` VALUES ('1', '1', '3333', '3212', '321', '312', '312', '312', '1', '1587366595', '1587367774', '1');
+INSERT INTO `p_pay_channel_account` VALUES ('1', '1', '3333', '3212', '321', '312', '312', '', '312', '1', '1587366595', '1587367774', '1');
 
 -- ----------------------------
 -- Table structure for `p_pay_order`
@@ -423,33 +425,34 @@ CREATE TABLE `p_pay_order` (
   `public_key` varchar(255) NOT NULL,
   `private_key` varchar(255) NOT NULL,
   `user_id` int(10) NOT NULL,
-  `user_account` int(10) unsigned NOT NULL,
+  `user_account` varchar(255) NOT NULL,
   `sys_order_id` varchar(100) NOT NULL,
   `user_order_id` varchar(100) NOT NULL,
-  `supplier_order_id` varchar(100) NOT NULL,
+  `supplier_order_id` varchar(100) NOT NULL DEFAULT '',
   `profit_rate` int(11) unsigned NOT NULL DEFAULT '0',
   `cost_rate` int(11) NOT NULL,
   `pay_money` int(11) unsigned NOT NULL DEFAULT '0',
-  `user_money` int(11) unsigned NOT NULL DEFAULT '0',
-  `cost_money` int(11) unsigned NOT NULL DEFAULT '0',
-  `profit_money` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_money` decimal(12,3) unsigned NOT NULL DEFAULT '0.000',
+  `cost_money` decimal(12,3) unsigned NOT NULL DEFAULT '0.000',
+  `profit_money` decimal(12,3) unsigned NOT NULL DEFAULT '0.000',
   `inform_num` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `user_notify_url` varchar(255) NOT NULL,
   `user_callback_url` varchar(255) NOT NULL,
-  `user_extra_field` varchar(255) NOT NULL,
+  `user_extra_field` varchar(255) NOT NULL DEFAULT '',
+  `user_sign_type` varchar(255) NOT NULL DEFAULT 'md5',
   `status` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `created_at` int(10) unsigned NOT NULL DEFAULT '0',
   `updated_at` int(10) unsigned NOT NULL DEFAULT '0',
-  `notify_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `success_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `query_time` int(10) unsigned NOT NULL DEFAULT '0',
+  `notify_at` int(10) unsigned NOT NULL DEFAULT '0',
+  `success_at` int(10) unsigned NOT NULL DEFAULT '0',
+  `query_at` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of p_pay_order
 -- ----------------------------
-INSERT INTO `p_pay_order` VALUES ('1', '4', '2', '213', '13123213214214', '1421421', '42141', '41421', '42141241', '1', '241421421', '12345678901234567890', '12345678901234567890', '12345678901234567890', '111', '111', '111111', '11111', '1111', '11111', '3', '1313131', '3131231', '31231', '5', '0', '0', '0', '0', '0');
+INSERT INTO `p_pay_order` VALUES ('2', '3', '1', 'Demo', '3333', '', '321', '312', '312', '10', 'lXxfn20200506190108', '102020050918442689392', 'abcdefjhijk123456', '1234567890', '1', '1', '100', '99.900', '0.100', '0.100', '1', 'http://api.localhostz/payment/test', 'http://api.localhost/payment/index', '', 'md5', '2', '1589021066', '1589021127', '1589021126', '1589021127', '0');
 
 -- ----------------------------
 -- Table structure for `p_product`
@@ -483,23 +486,28 @@ CREATE TABLE `p_user` (
   `auth_key` varchar(32) NOT NULL,
   `password_hash` varchar(256) NOT NULL,
   `password_reset_token` varchar(256) DEFAULT NULL,
+  `pay_password_hash` varchar(256) NOT NULL,
+  `pay_password_reset_token` varchar(256) DEFAULT NULL,
   `email` varchar(256) NOT NULL,
   `account` varchar(255) NOT NULL COMMENT '账号',
   `pay_md5_key` varchar(255) NOT NULL,
-  `money` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '现金,单位:分',
+  `money` decimal(12,3) unsigned NOT NULL DEFAULT '0.000' COMMENT '现金,单位:分',
   `status` int(11) NOT NULL DEFAULT '10',
+  `pre_login_ip` varchar(255) NOT NULL DEFAULT '',
   `pre_login_at` int(10) unsigned NOT NULL DEFAULT '0',
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of p_user
 -- ----------------------------
-INSERT INTO `p_user` VALUES ('1', 'demo', 'eb2ceSlO_HduMj9dQWzk9rPJt2P0L-bJ', '$2y$13$jD0WBG.bp.YGREdhbG3bFuZvy.BbF6yHnoP0r1klY0KyfFXdRA7aq', 'OPuCblPSXO8yZPJgK_1586831793', 'test@test.com', 'abcdefghijklmnopqrstuvwxyz', '10ebebdad7f7f3aa424372942633db8d', '11111', '10', '1588241391', '0', '1588241391');
-INSERT INTO `p_user` VALUES ('7', 'demo2', '1I8x_Bg8a2f3AelwzXj-j1aaNK_c2Zdt', '$2y$13$YRgTMvOVrVELCkUoZ.Qb3eOUzoUNwR0UzkleoKIS6ezyf.KJf2ohu', 'yDxRUVVfLzDJkDTGZ4HyGLsRYxkzPpXU_1588212892', 'aaa@a.com', 'qzk2h20200430101454', '10ebebdad7f7f3aa424372942633db8f', '0', '10', '0', '1588212892', '1588212892');
-INSERT INTO `p_user` VALUES ('8', 'demo3', 'e9IS3Z6yNUBos1d4bhbI-r8mEaTEPWyZ', '$2y$13$j.lsYcvKPkkRADQr71HO5u90udCiOU6DYtauJwGPpy4tYIWQMFpia', 'VmoV-AeDvv1do9AW5EfPd9wXDmgjEycp_1588213911', 'aaa2@a.com', 'cAaZh20200430103154', '8fafdd316542a68d3794c663bf3a80a8', '0', '1', '0', '1588213911', '1588213911');
+INSERT INTO `p_user` VALUES ('1', 'demo', 'Aqfq6YB47dhhM6JVqILP9BOVqRjZ3Ir5', '$2y$13$7vGMopJaq1pdGyr0TKEfcedVNH0TskmemAmEpFjqJGlMJsGASqrQy', 'blM_lP6OwLFmioDUGnZct8KLWhaP35nE_1588817982', '$2y$13$SuXdUDDknev0wU1EE7hPLeO3KJ6F8iyQkMSJP3p4QnLnBji/XuqSi', '0D4unvf9l6t2biMBCshOqqhOv6qP1DSP_1588731553', 'test@test.com', 'abcdefghijklmnopqrstuvwxyz', '10ebebdad7f7f3aa424372942633db8d', '11111.000', '10', '', '1588820170', '0', '1588820170');
+INSERT INTO `p_user` VALUES ('7', 'demo2', '1I8x_Bg8a2f3AelwzXj-j1aaNK_c2Zdt', '$2y$13$YRgTMvOVrVELCkUoZ.Qb3eOUzoUNwR0UzkleoKIS6ezyf.KJf2ohu', 'yDxRUVVfLzDJkDTGZ4HyGLsRYxkzPpXU_1588212892', '', null, 'aaa@a.com', 'qzk2h20200430101454', '10ebebdad7f7f3aa424372942633db8f', '0.000', '10', '', '1588729504', '1588212892', '1588729504');
+INSERT INTO `p_user` VALUES ('8', 'demo3', 'e9IS3Z6yNUBos1d4bhbI-r8mEaTEPWyZ', '$2y$13$j.lsYcvKPkkRADQr71HO5u90udCiOU6DYtauJwGPpy4tYIWQMFpia', 'VmoV-AeDvv1do9AW5EfPd9wXDmgjEycp_1588213911', '', null, 'aaa2@a.com', 'cAaZh20200430103154', '8fafdd316542a68d3794c663bf3a80a8', '0.000', '1', '', '0', '1588213911', '1588213911');
+INSERT INTO `p_user` VALUES ('9', 'demo2333', 'dgc2VjRBE7AhG-GqAd9hpjX2cpdmPxnF', '$2y$13$syLvF/v9F5P7.ARLD84DPe5k/2i4tYbTofgLscUFvHSBkuybzpc2i', 'xLTv8LqoXt-SYKqN9bCQVNwVrZyVkgUD_1588762456', '$2y$13$9CjsNp3UqM72gweBhObAku1Yt/1Du/saWlwUSH6o5vONv6Iy5EXyO', '3tK6dTm7F5ZhfPycOc0PTNHjVnJZtLCJ_1588762457', 'aaa2333@a.com', 'hyM5420200506185421', '8fb5e2b6fce2eb91c1d7459e7245ad08', '0.000', '1', '', '0', '1588762457', '1588762457');
+INSERT INTO `p_user` VALUES ('10', 'demob', 'M6QULc7Js0gpKnvrDfJpzTSMriv3m2AE', '$2y$13$2lbvSB038XTo8w1Tubarj.3UMDwYcKhUmCF/V5inslAkeO2/23kXu', 'dV5eSbd3uj_FI0EIB97JU5gvDSM8iWDk_1588762862', '$2y$13$lUOUzgp80qrhL1a6uaCJauedG5UbXcx3L0JgsWAJp9cBaRC1C2k6K', 'c4-ar59jKuZwngK3x8XnANEpoCUyJUZb_1588762863', 'bbb@a.com', 'lXxfn20200506190108', '3025ecc65c6882f1fcbbfee867c09731', '99.900', '10', '', '1588764217', '1588762863', '1588764217');
 
 -- ----------------------------
 -- Table structure for `p_user_to_pay_channel`
@@ -514,10 +522,11 @@ CREATE TABLE `p_user_to_pay_channel` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `pay_channel_id` (`pay_channel_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
 -- Records of p_user_to_pay_channel
 -- ----------------------------
 INSERT INTO `p_user_to_pay_channel` VALUES ('4', '1', '1', '1588237754', '1588237754');
 INSERT INTO `p_user_to_pay_channel` VALUES ('5', '1', '3', '1588237754', '1588237754');
+INSERT INTO `p_user_to_pay_channel` VALUES ('11', '10', '1', '1588990320', '1588990320');
